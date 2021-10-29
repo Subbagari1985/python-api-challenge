563ae0134f0ed30de74f97208a44f4f752043e92
Swapna S <subbagari.swapna@gmail.com>
2021-10-28 22:37:46 -0400

    Updated the file

--- WeatherPy/starter_code/api_keys.py
+++ WeatherPy/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "23dea60bf393fc24b73de54087cf1050"
+weather_api_key = "45e804ff91673b79da456b71cf0654c2"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBdw_vDcecNQnk7TQkWKDIO33-dfeYaPiM"
